0c4a6f7d0d8416ff0a93e972c9c0b4bfea8a63c1
priyanka981 <priyankachin17@gmail.com>
2024-05-09 13:13:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const Jsxheading = () => {
+    return <h1>hello world!!!React sk </h1>; }
+
+const root= ReactDom.createRoot(document.getElementById("root"));
+root.render(<Jsxheading/>);            
+        
